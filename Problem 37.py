ed5f1f2ea758682515a3085aab9dc646493dfb80
Tim Burimskiy <tim101206@mail.ru>
2021-09-12 17:44:38 +0200

    Final version

--- Problem 37.py
+++ Problem 37.py
@@ -1,14 +1,13 @@
 def is_prime(num):
-	a = all(num % i for i in range(2, num))
-	return a
+        a = all(num % i for i in range(2, num))
+        return a
 
-def is_prime_gen(num):
-	a = all(num % i for i in range(2, num))
-	if a == True:
-        return num
-
-    else:
-        return None
+def primes(n):
+    sieve = [True] * n
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
+    return [2] + [i for i in range(3,n,2) if sieve[i]]
 
 def truncatable_left(num):
     num = [i for i in str(num)]
@@ -44,10 +43,9 @@ def truncatable_right(num):
                 final = all([is_prime(num) for num in nums])
                 return final
 
-# nums = [int(x) for x in range(8, 750000) if is_prime(x) == True]
-# right = [i for i in nums if truncatable_right(i) == True]
-# left = [i for i in nums if truncatable_left(i) == True]
-# final = [x for x in nums if x in right if x in left]
-# print(final)
-nums = list(map(is_prime_gen, range(8, 1000)))
-print(nums)
+nums = primes(750000)
+right = [i for i in nums if truncatable_right(i) == True]
+left = [i for i in nums if truncatable_left(i) == True]
+banned = [2, 3, 5, 7]
+final = [x for x in nums if x in right if x in left if x not in banned]
+print(sum(final))
