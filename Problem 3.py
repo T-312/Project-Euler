0ab4fd4e5931090f2f929c4069a3a7e7446dac9c
Tim Burimskiy <tim101206@mail.ru>
2021-09-13 18:55:04 +0200

    Fixed older code - this version might require optimization, but works correctly

--- Problem 3.py
+++ Problem 3.py
@@ -1,30 +1,21 @@
-# num = 600851475143
-# factors = [x for x in range(2, num) if num % x == 0]
-# def exclude(x):
-#     for num in factors:
-#         if num > x and num % x == 0:
-#             factors.remove(num)
+def is_prime(num):
+    import math
+    if num <= 2:
+      return True
+    if not num % 2:
+      return False
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+      if not num % i  and num != i:
+          return False
+    return True
 
-# def loop():
-#     exclude(2)
-#     exclude(3)
-#     exclude(5)
-#     exclude(7)
-#     exclude(11)
-#     exclude(13)
+def gen_prime_factors(num):
+    factors = []
+    for i in range(2, num):
+        if num % i == 0 and is_prime(i):
+            if i not in factors:
+                factors.append(i)
 
-# loop()
-# loop()
+    return factors
 
-# print(factors[-1])
-
-def primes(num):
-    a = [i for i in range(1, num) if num % i == 0]
-    for i in a:
-        for n in range(i+1, num):
-            if i % n in a:
-                a.remove(n)
-
-    print(a)
-
-primes(13195)
+print(max(gen_prime_factors(600851475143)))
