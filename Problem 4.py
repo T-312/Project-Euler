93991fedbd3dd85f8cfebbba106060d44b7c8e6f
Tim Burimskiy <tim101206@mail.ru>
2021-09-13 20:05:24 +0200

    Final version

--- Problem 4.py
+++ Problem 4.py
@@ -1,12 +1,7 @@
+def is_palindrome(num):
+    rev = int(str(num)[::-1])
+    return rev == num
+
 nums = list(range(100, 1000))
-final = []
-
-for a in nums:
-    for b in nums:
-        for c in nums:
-            d = a * b * c
-            if str(d) == str(d)[::-1]:
-                final.append(d)
-
-final.sort()
-print(final[-1])
+prods = [a*b for a in nums for b in nums if is_palindrome(a*b)]
+print(max(prods))
