b50d81f6af4cc4ba3c775670ffb8597ecc6c33e8
Tim <tim101206@mail.ru>
2021-09-14 09:57:02 +0200

    Code works - needs optimization

--- Problem 5.py
+++ Problem 5.py
@@ -1,18 +1,19 @@
-num = 1
-divisors = []
-non_divisors = []
-while len(non_divisors) != 0:
-    for i in range(1, 10):
-        if num % i == 0:
-            divisors.append(i)
+def gen_multiples(num):
+	multiples = []
+	for i in range(1, num):
+		if num % i == 0:
+			multiples.append(i)
 
-        else:
-            non_divisors.append(i)
-            num+=1
+	return multiples
 
-    if len(non_divisors) == 0:
-        print(divisors)
-    
-    else:
-        divisors.clear()
-        non_divisors.clear()
+def check_nums(multiples):
+	nums = list(range(1, 20+1))
+	num_range = []
+	for i in nums:
+		if i in multiples:
+			num_range.append(int(i))
+
+	return num_range == nums
+
+nums = [x for x in range(1, 10000) if check_nums(gen_multiples(x))]
+print(nums)
