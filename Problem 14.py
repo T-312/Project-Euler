365d97bdbb59c82b88e996ea9412329eb381d8e7
Tim Burimskiy <tim101206@mail.ru>
2021-09-14 16:50:42 +0200

    Fixed problem 14 and started working on problem 44

--- Problem 14.py
+++ Problem 14.py
@@ -1,9 +1,7 @@
 def func(num):
-	seq = []
-	if num < 2:
-		return seq
-
-	else:
+	seq = [num]
+	
+	while num != 1:
 		if num % 2 == 0:
 			num = num / 2
 			seq.append(int(num))
@@ -11,4 +9,17 @@ def func(num):
 		else:
 			num = 3 * num + 1
 			seq.append(int(num))
-print(func(15))
+
+	return seq
+
+longest_chain = 0
+lg_start_num = 0
+
+for n in range(1, 1000001):
+	a = func(n)
+	b = len(func(n))
+	if b > longest_chain:
+		longest_chain = b
+		lg_start_num = a[0]
+
+print(lg_start_num)
--- Problem 44.py
+++ Problem 44.py
@@ -4,7 +4,16 @@ def formula(n):
 	return n*(3*n-1)/2
 
 def check_sum(nums):
-	return [str(int(a))+str(int(b)) for a in nums for b in nums if a+b in nums]
+	sums = [[a, b, a+b] for a in nums for b in nums if a != b and a+b in nums]
+	return sums
 
-pent_nums = list(map(formula, range(1, 100)))
-print(check_sum(pent_nums))
+def check_dif(nums):
+	difs = [[a, b, a - b] for a in nums for b in nums if a != b and a-b in nums]
+	return difs
+
+pent_nums = list(map(formula, range(1, 1000)))
+difs = [x for x in check_dif(pent_nums)]
+sums = [x for x in check_sum(pent_nums)]
+final = [x for x in pent_nums if x in difs and x in sums]
+
+print(final)
