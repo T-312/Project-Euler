b61e4b1e088d2907367423d6803030582f1d9ac6
Tim <tim101206@mail.ru>
2021-09-14 12:43:42 +0200

    Final version - takes about 2 minutes to finish execution

--- Problem 7.py
+++ Problem 7.py
@@ -1,39 +1,10 @@
-nums = list(range(3, 105000))
+def is_prime(num):
+	if num <= 2:
+		return True
 
-def prime(num):
-    a = []
-    if num > 1:
-        for i in range(2, num):
-            if num > 3:
-                if num % 3 == 0:
-                    a.append('no')
-            if num > 5:
-                if num % 5 == 0:
-                    a.append('no')
-            
-            if num > 7:
-                if num % 7 == 0:
-                    a.append('no')
+	else:
+		a = all(num % n for n in range(2, num))
+		return a
 
-            if num % i == 0:
-                a.append('no')
-
-            else:
-                a.append(str(num))
-
-    else:
-        a.append('no')
-
-    return a[0]
-    a.clear()
-
-print("Making list...")
-main = list(map(prime, nums))
-main.insert(0, '2')
-print("Removing the 'no' from the list...")
-while 'no' in main:
-    main.remove('no')
-
-print(main[10000])
-
-#104743
+a = [n for n in range(1, 105000) if is_prime(n)]
+print(a[10001])
