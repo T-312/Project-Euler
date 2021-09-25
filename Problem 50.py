f48e5360fafb9f87e50c372beb2c93949e83e086
Tim Burimskiy <tim101206@mail.ru>
2021-09-25 20:32:02 +0200

    -

--- Problem 50.py
+++ Problem 50.py
@@ -19,19 +19,19 @@ def gen_primes(num):
 	return primes
 
 start = time.time()
-limit = 100
+limit = 100000
 length = 0
 value = 0
 nums = gen_primes(limit)
 
-for a in range(len(nums)):
+for a in range(round(len(nums)/3)):
 	for b in range(1, len(nums)):
 		x = nums[a:b]
 		if len(x) > length and sum(x) < limit and is_prime(sum(x)):
 			length = len(x)
 			value = sum(x)
 			print(value)
-			for i in range(int(limit/1000)):
+			for i in range(int(limit/200)):
 				nums.pop()
 
 stop = time.time()
